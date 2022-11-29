06ed6a384af2f9cc8fe90200455285e80b486f28
hasiniaina <hasiniaina.faramalala@gmail.com>
2022-11-29 17:14:24 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -11,3 +11,29 @@ let mdp = document.getElementById("mdp").value;
     }
 
 }
+
+function getPersonne(){
+    let nom = document.getElementById("anarana").value;
+    let prenom = document.getElementById("fanampiny").value;
+    let date = document.getElementById("daty").value;
+    let profession = document.getElementById("asa").value;
+    let situation;
+    if(document.getElementById("marie").checked){
+        situation = document.getElementById("marie").value;
+    }
+    else {
+        situation = document.getElementById("celibataire").value;
+    }
+
+    let sexe;
+    if(document.getElementById('feminin').checked){
+        sexe = document.getElementById('feminin').value;
+    }
+    else {
+       sexe = document.getElementById("masculin").value;
+    }
+
+    alert(" Je m'appelle " +nom + " " + prenom + ".\n Je suis néé(e) le " +date + " du sexe " +sexe+ " . \n Ma situation familiale est " + situation + " sans activité.");
+    
+    
+}
